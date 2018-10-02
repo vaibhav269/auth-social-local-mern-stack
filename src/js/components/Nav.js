a24b4e3ce11479d28dd4742585cdb11dc93b096b
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-02 20:09:03 +0530

    added styles for homepage

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -4,14 +4,16 @@ import {Link} from 'react-router-dom';
 class Nav extends Component{
     render(){
         return(
-            <nav className="navbar navbar-dark navbar-expand bg-dark col-12 w-100" >
-                <ul className="navbar-nav">
-                    <li > <Link to = '/' className="navbar-brand" >Node Auth</Link></li>
-                    <li className="nav-item"> <Link to = '/login' className="nav-link"> Login</Link> </li>
-                    <li className="nav-item"> <Link to = '/signup' className="nav-link"> Signup</Link> </li>
-                    <li className="nav-item"> <Link to = '/' className="nav-link"> Home</Link> </li>
-                </ul>
-            </nav>
+            <div className="w-100">
+                <nav className="navbar navbar-dark navbar-expand bg-dark col-12 w-100" >
+                    <ul className="navbar-nav">
+                        <li > <Link to = '/' className="navbar-brand" >Node Auth</Link></li>
+                        <li className="nav-item"> <Link to = '/login' className="nav-link"> Login</Link> </li>
+                        <li className="nav-item"> <Link to = '/signup' className="nav-link"> Signup</Link> </li>
+                        <li className="nav-item"> <Link to = '/' className="nav-link"> Home</Link> </li>
+                    </ul>
+                </nav>
+            </div>
         )
     }
 }
