04f7f1d035bcf315260db5cc7296f1fe126b2409
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-04 19:26:56 +0530

    handled error messages and isLoading state

--- src/js/components/Signup.js
+++ src/js/components/Signup.js
@@ -5,10 +5,18 @@ class Signup extends Component{
     
     constructor(){
         super();
+        this.state = {
+            isLoading:false,
+            signupError:''
+        }
+        this.localSignup = this.localSignup.bind(this);
     }
 
     localSignup(event){
         event.preventDefault();
+
+        this.setState({isLoading:true});
+
         const formData = new FormData(event.target);
         let jsonObject = {};
 
@@ -26,20 +34,30 @@ class Signup extends Component{
         fetch('http://localhost:3000/api/account/signup',options)
         .then( res=>{
             res.json().then(
-                (text)=>{ console.log(text);}
+                (text)=>{                     
+                    this.setState({isLoading:false});                    
+                    if(text.success == false){
+                        this.setState({signupError:text.message});                        
+                    }
+                }
             );
         })
     }
     
     render(){
-        return(
-            <div className="col-lg-3 mt-lg-5 border border-dark" >
-                
+        let {isLoading,signupError} = this.state;
+        console.log("sd",signupError);
+        return(            
+            <div className="col-lg-3 mt-lg-5 border border-dark" >               
                 <div className="row bg-dark p-1"
                     style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",fontWeight:"bolder",fontSize:"150%"}}>
                     <p className="w-100 text-center m-0">Signup</p>
                 </div>
                 
+                <div className = "row">
+                    <p className = "bg-danger text-white w-100 text-center"> {signupError} </p>                
+                </div>
+
                 <div className="row justify-content-center pr-3 pl-3 mt-3">
                     <form className="w-100" onSubmit = {this.localSignup}>
                         <div className = "form-group" >
@@ -61,7 +79,9 @@ class Signup extends Component{
                 <hr className = "m-3"/>
                 <p>Already have an account? <Link to="/login">Login</Link></p>
                 <p>Or go <Link to="/">Home</Link>.</p>
-
+                {
+                    (isLoading)?(<p>Loading.....</p>):(<p></p>)
+                }
             </div>
         )
     }
