7860e857e7bfea64d377cbf7a73ae5fefb11b6b0
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-05 12:04:59 +0530

    added logout route

--- server/app/routes/api/signin.js
+++ server/app/routes/api/signin.js
@@ -122,4 +122,28 @@ module.exports = function(app){
 
         });
     });
+
+    app.get('/api/account/logout',function(req,res,next){
+        const {query} = req.query;
+        UserSession.findOneAndUpdate({
+            _id:token,
+            isDeleted:false
+        },{
+            $set:{
+                isDeleted:true
+            }
+        },null,function(err,sessions){
+            if(err){
+                console.log(err);
+                return res.send({
+                    success:false,
+                    message:'Error:Serve error'
+                });
+            }
+            return res.send({
+                success:true,
+                message:'Logged Out Successfully'
+            });
+        });
+    });
 }
