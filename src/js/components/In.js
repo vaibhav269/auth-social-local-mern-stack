a9771c86e74463540add5bc5bd74e23ed06fb1f7
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-10 16:40:37 +0530

    Added Navigation in In component

--- src/js/components/In.js
+++ src/js/components/In.js
@@ -7,11 +7,91 @@ import {
     Redirect
 } from 'react-router-dom';
 
+import Profile from './Profile';
+import Nav from './Nav';
+
 class In extends Component{    
-    render(){
+    constructor(){
+        super();        
+
+        this.state = {
+            token : '',
+            navSessionButtonData :[],
+            navSessionRouteData:[],
+            isLoading:false            
+        }
+        this.isLoading = this.isLoading.bind(this);
+    }
+
+    logout(){
+        alert("logged out");
+    }
+
+    componentDidMount(){
+        let {match} = this.props;
+        this.setState({
+            navSessionRouteData : [                
+                {to:`${match.url}`,name:'Profile',key:'r3'}
+            ],
+            navSessionButtonData : [{onClick:this.logout,name:'Logout',key:'b1'}],
+            isLoading:true
+        }) 
+
+        const tokenVar = localStorage.getItem('token');
+        if(tokenVar == null){
+            this.setState({
+                isLoading:false
+            });
+        }else{            
+            fetch('http://localhost:3000/api/account/verify?token='+tokenVar)
+            .then(res=>res.json())
+            .then(json=>{
+                if(json.success){
+                    this.setState({
+                        token : tokenVar,
+                        isLoading:false
+                    });                    
+                }else{
+                    this.setState({                 
+                        isLoading:false,
+                    });
+                }
+            });
+        }
+    }
+    
+    isLoading(){
+        let {isLoading,token,navSessionButtonData,navSessionRouteData} = this.state;
+        if(isLoading === true){
+            return (
+                <p>Loading...</p>
+            );
+        }
+        else{
+            let {match} = this.props            
+            return(
+                <div>
+                    {
+                        (token)?(null):<Redirect to={{pathname: '/In', state: {from: this.props.location}}} />
+                    }
+                    <div className = "row">
+                        <Nav navRouteData = {navSessionRouteData}  navButtonData = {navSessionButtonData}/>
+                    </div>
+                        <div className="row justify-content-center">
+                            <Switch>
+                                <Route exact = {true} path={`${match.path}`} component={Profile} />                                
+                            </Switch>
+                        </div>
+                </div>
+            )
+        }
+    }
+
+
+    render(){    
         return(
             <div>
-                <h1>In Component !!</h1>
+                {this.isLoading()}
             </div>
         )
     }
