968f711e7c1df2d0a280925754948dadb212d2ec
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-11 00:25:19 +0530

    fixed crash on reloading problem by replacing browser router with hashrouter

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,15 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import store from  './store/index';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppOne from './components/AppOne';
 //window.store = store;
 
 render(    
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <AppOne />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>,
     document.getElementById('app')
 );
