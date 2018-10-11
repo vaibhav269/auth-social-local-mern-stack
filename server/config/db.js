e7cff66705d6e9a0326b2a53409f05d32e247b76
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-12 00:13:42 +0530

    added remote db url

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'url':'mongodb://localhost/authPassport'
+    //'url':'mongodb://localhost/authPassport'
+    'url':'mongodb://vaibhav:vaibhav2697@ds229373.mlab.com:29373/authpassport'    
 }
