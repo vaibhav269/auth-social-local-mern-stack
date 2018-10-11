18e2855191cc9489a215a3a8b00356611a9c338d
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-11 23:31:29 +0530

    made profile component dynamic based on user's data

--- src/js/components/Profile.js
+++ src/js/components/Profile.js
@@ -2,24 +2,38 @@ import React,{Component} from 'react';
 
 class Profile extends Component{
     constructor(){
-        super();        
+        super();
+        this.state={                       
+        }
     }
     
-    componentDidMount(){
+    componentWillMount(){
         const tokenVar = localStorage.getItem('token');
         fetch('/api/account/getUserData?token='+tokenVar)
             .then(res=>res.json())
             .then(json=>{
-                console.log(json);
+                if(json.success === true){
+                    console.log(json.message.email);
+                    this.setState({
+                        email:json.message.email,
+                        signUpDate:json.message.signUpDate
+                    });
+                    console.log(this.state);
+                }
+                else{
+                    console.log('error occured');
+                    alert(json.message);
+                }
             });
     }
 
-    render(){        
+    render(){   
+        let {email,signUpDate} = this.state;        
         return(
             <div className="row">
                 <div className="col-12">
-                    <div>Name : Vaibhav</div>
-                    <div>Work : Developing Web Applications</div>
+                    <div>email : {email}</div>
+                    <div>signUpDate : {signUpDate}</div>
                 </div>
             </div>
         )
