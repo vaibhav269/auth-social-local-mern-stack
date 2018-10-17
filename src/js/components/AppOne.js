a1cc06ff77f8c6080a3b40f449c70624c23f3ece
Vaibhav <vaibhav.verma2697@gmail.com>
2018-10-17 13:56:09 +0530

    Added preloaded

--- src/js/components/AppOne.js
+++ src/js/components/AppOne.js
@@ -13,42 +13,21 @@ import Out from '../components/Out';
 class AppOne extends Component{
     constructor(){
         super();
-        this.state = {            
-            isLoading:true
-        }
-        this.isLoading = this.isLoading.bind(this);
     }
     
-    componentDidMount(){
-        this.setState({isLoading:false});
-    }
-
-    isLoading(){
-        let {isLoading} = this.state;
-
-        if(isLoading === true){
-            return (
-                <p>Loading...</p>
-            );
-        }
-        else{
-            return(                
-                    <div className="container-fluid">
-                        <Switch>                            
-                            <Route path='/Out' component = {Out} />
-                            <Route path='/In' component = {In} />
-                            <Route path='/' component = {Out} />
-                        </Switch>
-                    </div>
-            )
-        }
+    componentDidMount(){        
+        document.getElementById('preloader').style.display = "none";            
     }
 
     render(){        
-        return(
-            <div>
-                {this.isLoading()}
-            </div>
+        return(            
+            <div className="container-fluid">
+                <Switch>                            
+                    <Route path='/Out' component = {Out} />
+                    <Route path='/In' component = {In} />
+                    <Route path='/' component = {Out} />
+                </Switch>
+            </div>            
         )
     }
 }
